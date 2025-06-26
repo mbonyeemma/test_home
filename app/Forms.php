a0cf56c709d3eb22d0bdf2738ec2d7df6c4a6ac4
Joseph Gibusiwa <JGibusiwa@pridebank.co.ug>
2025-06-26 09:21:56 +0300

    edit forms and form fields

--- app/Forms.php
+++ app/Forms.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Forms extends Model
 {
-    protected $fillable = ['name', 'form_id'];
+    protected $fillable = ['name', 'form_id', 'form_submission_url'];
     public function fields()
     {
         return $this->hasMany(FormFields::class);
